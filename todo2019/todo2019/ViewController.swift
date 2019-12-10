88de2b8dfd6237aad316f86273bc0e092afb36c3
sekitaka <sekitaka.tech@gmail.com>
2019-12-10 15:42:58 +0900

    LaunchScreenのテキストをAutoLayoutでどんなサイズのデバイスでも中央に表示する

---
+++ todo2019/todo2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo2019
+//
+//  Created by Takashi Seki on 2019/12/10.
+//  Copyright © 2019 Takashi Seki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
